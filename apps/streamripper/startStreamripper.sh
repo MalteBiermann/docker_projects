4057e70309a7cd65925f459c7898d78251bd1a9b
Malte Biermann <51059791+MalteBiermann@users.noreply.github.com>
2026-02-07 20:46:03 +0100

    changed devcontainer mount, output folder

--- apps/streamripper/startStreamripper.sh
+++ apps/streamripper/startStreamripper.sh
@@ -2,4 +2,4 @@
 set -eu
 
 date
-streamripper http://oeins.de:8000/oeins -d /workspace/streamripper/music/ -s -r 8001 -l "${1}" -D '%d %S %A %T'
+streamripper http://oeins.de:8000/oeins -d /workspace/streamripper/output/music/ -s -r 8001 -l "${1}" -D '%d %S %A %T'
